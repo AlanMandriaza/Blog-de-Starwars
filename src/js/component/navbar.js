c9f2efabbceab2861cfe5391b6234d3a2e71d266
AlanMandriaza <alan.mandriaza@gmail.com>
2023-03-17 04:33:34 +0000

    avanzando

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,30 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">Star Wars API</span>
-			</Link>
-			<ul className="navbar-nav ml-auto">
-				<li className="nav-item">
-					<Link to="/people" className="nav-link">
-						People
-					</Link>
-				</li>
-				<li className="nav-item">
-					<NavLink to="/vehicles" className="nav-link">
-						Vehicles
-					</NavLink>
-				</li>
-				<li className="nav-item">
-					<Link to="/planets" className="nav-link">
-						Planets
-					</Link>
-				</li>
-			</ul>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/people" className="nav-link">
+                People
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/vehicles" className="nav-link">
+                Vehicles
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/planets" className="nav-link">
+                Planets
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
--- src/js/component/people.js
+++ src/js/component/people.js
@@ -37,22 +37,37 @@ function People() {
   return (
     <div>
       <h1>People</h1>
-      <div className="card-group">
-        {people.map((person) => (
-          <div className="card" style={{ width: "18rem" }} key={person.uid}>
-            <div className="card-body">
-              <h5 className="card-title">{person.name}</h5>
-              <p>Height: {person.properties.height}</p>
-              <p>Mass: {person.properties.mass}</p>
-              <p>Hair Color: {person.properties.hair_color}</p>
-              <p>Skin Color: {person.properties.skin_color}</p>
-              <p>Eye Color: {person.properties.eye_color}</p>
-              <p>Birth Year: {person.properties.birth_year}</p>
-              <p>Gender: {person.properties.gender}</p>
-              <p>Created: {person.properties.created}</p>
+      <div className="container-fluid">
+        <div className="row">
+          {people.map((person) => (
+            <div className="col-md-4 col-lg-3 mb-4" key={person.uid}>
+              <div className="card h-100">
+                <img
+                  src="https://lumiere-a.akamaihd.net/v1/images/luke-skywalker-main_fb34a1ff.jpeg?region=131%2C0%2C951%2C536"
+                  className="card-img-top"
+                  alt="Luke Skywalker"
+                  style={{ height: 250, width: 250, objectFit: 'cover' }}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{person.name}</h5>
+                  <p className="card-text">Height: {person.properties.height}</p>
+                  <p className="card-text">Mass: {person.properties.mass}</p>
+                  <p className="card-text">Hair Color: {person.properties.hair_color}</p>
+                  <p className="card-text">Skin Color: {person.properties.skin_color}</p>
+                  <p className="card-text">Eye Color: {person.properties.eye_color}</p>
+                  <p className="card-text">Birth Year: {person.properties.birth_year}</p>
+                  <p className="card-text">Gender: {person.properties.gender}</p>
+                  <p className="card-text">Created: {person.properties.created}</p>
+                  <div className="card-footer">
+                    <a href="#" className="card-link">
+                      Learn More
+                    </a>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
