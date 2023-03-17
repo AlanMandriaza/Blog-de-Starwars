fea9e57d69d571fbd25b8d69281df3c5f4925240
AlanMandriaza <alan.mandriaza@gmail.com>
2023-03-17 06:38:08 +0000

    people casi terminado

--- src/js/component/people.js
+++ src/js/component/people.js
@@ -37,38 +37,55 @@ function People() {
   return (
     <div>
       <h1>People</h1>
-      <div className="container-fluid">
-        <div className="row">
-          {people.map((person) => (
-            <div className="col-md-4 col-lg-3 mb-4" key={person.uid}>
-              <div className="card h-100">
-                <img
-                  src="https://lumiere-a.akamaihd.net/v1/images/luke-skywalker-main_fb34a1ff.jpeg?region=131%2C0%2C951%2C536"
-                  className="card-img-top"
-                  alt="Luke Skywalker"
-                  style={{ height: 250, width: 250, objectFit: 'cover' }}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{person.name}</h5>
-                  <p className="card-text">Height: {person.properties.height}</p>
-                  <p className="card-text">Mass: {person.properties.mass}</p>
-                  <p className="card-text">Hair Color: {person.properties.hair_color}</p>
-                  <p className="card-text">Skin Color: {person.properties.skin_color}</p>
-                  <p className="card-text">Eye Color: {person.properties.eye_color}</p>
-                  <p className="card-text">Birth Year: {person.properties.birth_year}</p>
-                  <p className="card-text">Gender: {person.properties.gender}</p>
-                  <p className="card-text">Created: {person.properties.created}</p>
-                  <div className="card-footer">
-                    <a href="#" className="card-link">
-                      Learn More
-                    </a>
-                  </div>
-                </div>
+      
+
+      <div className="container py-5">
+  <div className="d-flex flex-nowrap overflow-auto">
+    {people.map((person) => (
+      <div className="col-12" key={person.uid}>
+        <div className="card mb-5" style={{ width: "800px" }}>
+          <div className="row g-0">
+            <div className="col-md-6">
+              <img
+                src="https://dojiw2m9tvv09.cloudfront.net/10102/product/c3po-39845.jpg"
+                className="img-fluid rounded-start"
+                alt="..."
+              />
+            </div>
+            <div className="col-md-6">
+              <div className="card-body">
+                <h5 className="card-title">{person.name}</h5>
+                <p className="card-text">
+                  Height: {person.properties.height}
+                </p>
+                <p className="card-text">Mass: {person.properties.mass}</p>
+                <p className="card-text">
+                  Hair Color: {person.properties.hair_color}
+                </p>
+                <p className="card-text">
+                  Skin Color: {person.properties.skin_color}
+                </p>
+                <p className="card-text">
+                  Eye Color: {person.properties.eye_color}
+                </p>
+                <p className="card-text">
+                  Birth Year: {person.properties.birth_year}
+                </p>
+                <p className="card-text">
+                  Gender: {person.properties.gender}
+                </p>
+                <p className="card-text">
+                  Created: {person.properties.created}
+                </p>
               </div>
             </div>
-          ))}
+          </div>
         </div>
       </div>
+    ))}
+  </div>
+</div>
+
     </div>
   );
 }
